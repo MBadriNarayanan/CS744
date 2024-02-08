8ea9e20a0eb932b31808145db1a39c0caabb7149
MBadriNarayanan <MBadriNarayanan>
2024-02-08 08:24:25 -0600

    [ UPDATE ] - Part Two Script

--- AssignmentOne/part_two.py
+++ AssignmentOne/part_two.py
@@ -1,44 +1,39 @@
 import sys
 
 from pyspark.sql.functions import col
-
-from utils.general_utils import rename_csv_file
 from utils.spark_utils import create_spark_application
 
 
-def sort_data(input_dataframe_path, output_dataframe_folder):
+def sort_data(input_dataframe_path, output_dataframe_path):
     """
     Function to sort the dataframe based
     on country_code and time stamp.
 
-    I/P: Input Dataframe Path and Output Dataframe Folder
+    I/P: Input Dataframe Path and Output Dataframe Path
     O/P: Sorts the dataframe based on given criteria and
-    renames it to output.csv.
+    saves it to HDFS.
     """
     spark = create_spark_application(application_name="SortData")
     df = spark.read.csv(input_dataframe_path, header=True)
     df = df.orderBy(col("cca2"), col("timestamp")).repartition(1)
-    df.write.csv(output_dataframe_folder, header=True)
+    df.write.csv(output_dataframe_path, header=True)
     spark.stop()
-    rename_csv_file(
-        input_directory=output_dataframe_folder, output_csv_name="output.csv"
-    )
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print(
-            "Make sure Input Dataframe path and Output Folder are passed as argument!"
+            "Make sure Input Dataframe path and Output Dataframe Path are passed as argument!"
         )
         print("\n--------------------\nExited the application!\n--------------------\n")
         sys.exit()
 
     input_dataframe_path = sys.argv[1]
-    output_dataframe_folder = sys.argv[2]
+    output_dataframe_path = sys.argv[2]
 
     sort_data(
         input_dataframe_path=input_dataframe_path,
-        output_dataframe_folder=output_dataframe_folder,
+        output_dataframe_path=output_dataframe_path,
     )
     print(
         "\n--------------------\nData has been sorted successfully!\n--------------------\n"
--- AssignmentOne/part_two.sh
+++ AssignmentOne/part_two.sh
@@ -4,7 +4,7 @@ SPARK_HOME=/users/mbadnara/spark-3.3.4-bin-hadoop3
 SPARK_MASTER_URL=spark://node0:7077
 
 PYTHON_SCRIPT=/users/mbadnara/CS744/AssignmentOne/part_two.py
-INPUT_DATAFRAME_PATH=/users/mbadnara/CS744/AssignmentOne/data/export.csv
-OUTPUT_DATAFRAME_FOLDER=/users/mbadnara/CS744/AssignmentOne/data/output
+INPUT_DATAFRAME_PATH=hdfs://10.10.1.1:9000/user/mbadnara/data/export.csv
+OUTPUT_DATAFRAME_PATH=hdfs://10.10.1.1:9000/user/mbadnara/data/output/output.csv
 
-$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER_URL $PYTHON_SCRIPT $INPUT_DATAFRAME_PATH $OUTPUT_DATAFRAME_FOLDER
+$SPARK_HOME/bin/spark-submit --master $SPARK_MASTER_URL $PYTHON_SCRIPT $INPUT_DATAFRAME_PATH $OUTPUT_DATAFRAME_PATH
