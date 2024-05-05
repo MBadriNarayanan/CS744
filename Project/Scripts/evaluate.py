23b35717a656beae186799fe5cf16834fbd26c4a
MBadriNarayanan <MBadriNarayanan>
2024-05-04 19:39:08 -0500

    [ UPDATE ] - Evaluate Script

--- Project/Scripts/evaluate.py
+++ Project/Scripts/evaluate.py
@@ -3,14 +3,21 @@ import json
 
 import torch
 
+from accelerate import Accelerator
 from datasets import load_dataset
-from utils import (
-    create_helper_directories,
-    get_data_loader,
-    prepare_base_model,
-    prepare_model_for_evaluation,
-    evaluate_model,
+from torch.utils.data import DataLoader
+from transformers import (
+    BertTokenizer,
+    BertForSequenceClassification,
+    MegatronBertForSequenceClassification,
 )
+from evaluate_utils import (
+    prepare_model_for_evaluation_on_single_gpu,
+    prepare_model_for_evaluation_on_multiple_gpu,
+    evaluate_model_on_single_gpu,
+    evaluate_model_on_multiple_gpu,
+)
+from general_utils import create_helper_directories
 
 
 def main(config):
@@ -32,6 +39,7 @@ def main(config):
 
     batch_size = config["Eval"]["batchSize"]
     checkpoint_path = config["Eval"]["checkpointPath"]
+    accelerate_checkpoint_dir = config["Eval"]["accelerateCheckpointDir"]
 
     (
         val_csv_path,
@@ -46,73 +54,143 @@ def main(config):
     )
 
     if torch.cuda.is_available():
-        device = torch.device("cuda")
-        print("CUDA available!")
-    elif torch.backends.mps.is_available():
-        device = torch.device("mps")
-        print("Metal available!")
+        num_gpus = torch.cuda.device_count()
+        if num_gpus == 1:
+            print("GPU Available!")
+            print("Number of GPUs present: {}!".format(num_gpus))
+            device = torch.device("cuda")
+            model = BertForSequenceClassification.from_pretrained(
+                model_name, num_labels=label_count
+            )
+            multi_gpu_flag = False
+        else:
+            accelerator = Accelerator()
+            device = accelerator.device
+            accelerator.print("GPU Available!")
+            accelerator.print("Number of GPUs present: {}!".format(num_gpus))
+            model = MegatronBertForSequenceClassification.from_pretrained(
+                "{}/{}".format("models", model_name), num_labels=label_count
+            )
+            multi_gpu_flag = True
     else:
+        print("GPU not available using CPU!")
         device = torch.device("cpu")
-        print("GPU not available, using CPU!")
+
+    (
+        val_csv_path,
+        val_report_path,
+        test_csv_path,
+        test_report_path,
+    ) = create_helper_directories(
+        checkpoint_dir=checkpoint_dir,
+        logs_dir=logs_dir,
+        task_name=task_name,
+        flag=False,
+        print_flag=multi_gpu_flag,
+    )
 
     torch.cuda.empty_cache()
 
-    model, tokenizer = prepare_base_model(
-        model_name=model_name, label_count=label_count
-    )
+    tokenizer = BertTokenizer.from_pretrained(model_name)
     dataset = load_dataset(dataset_class, dataset_name)
 
+    def tokenize_function(data):
+        return tokenizer(
+            data["sentence"],
+            max_length=max_length,
+            padding=padding_value,
+            truncation=truncation_flag,
+            return_tensors=return_tensors,
+            add_special_tokens=special_token_flag,
+        )
+
     val_data = dataset["validation"]
-    val_loader = get_data_loader(
-        data=val_data,
-        tokenizer=tokenizer,
-        max_length=max_length,
-        padding_value=padding_value,
-        truncation_flag=truncation_flag,
-        return_tensors=return_tensors,
-        special_token_flag=special_token_flag,
-        batch_size=batch_size,
-        shuffle_flag=shuffle_flag,
-    )
-
     test_data = dataset["test"]
-    test_loader = get_data_loader(
-        data=test_data,
-        tokenizer=tokenizer,
-        max_length=max_length,
-        padding_value=padding_value,
-        truncation_flag=truncation_flag,
-        return_tensors=return_tensors,
-        special_token_flag=special_token_flag,
-        batch_size=batch_size,
-        shuffle_flag=shuffle_flag,
-    )
 
-    model = prepare_model_for_evaluation(
-        model=model, device=device, checkpoint_path=checkpoint_path, flag="validation"
-    )
-    evaluate_model(
-        model=model,
-        device=device,
-        data_loader=val_loader,
-        report_path=val_report_path,
-        csv_path=val_csv_path,
-        checkpoint_path=checkpoint_path,
-        flag="validation",
-    )
+    if multi_gpu_flag:
+        with accelerator.main_process_first():
+            val_data = val_data.map(
+                tokenize_function, batched=True, remove_columns=["idx"]
+            )
+            test_data = test_data.map(
+                tokenize_function, batched=True, remove_columns=["idx"]
+            )
+    else:
+        val_data = val_data.map(tokenize_function, batched=True, remove_columns=["idx"])
+        test_data = test_data.map(
+            tokenize_function, batched=True, remove_columns=["idx"]
+        )
 
-    model = prepare_model_for_evaluation(
-        model=model, device=device, checkpoint_path=checkpoint_path, flag="testing"
-    )
-    evaluate_model(
-        model=model,
-        device=device,
-        data_loader=test_loader,
-        report_path=test_report_path,
-        csv_path=test_csv_path,
-        checkpoint_path=checkpoint_path,
-        flag="testing",
-    )
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=shuffle_flag)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=shuffle_flag)
+
+    if not multi_gpu_flag:
+        print("Evaluating model using a single GPU!")
+        model = prepare_model_for_evaluation_on_single_gpu(
+            model=model,
+            device=device,
+            checkpoint_path=checkpoint_path,
+            flag="validation",
+        )
+        evaluate_model_on_single_gpu(
+            model=model,
+            device=device,
+            data_loader=val_loader,
+            report_path=val_report_path,
+            csv_path=val_csv_path,
+            checkpoint_path=checkpoint_path,
+            flag="validation",
+        )
+        model = prepare_model_for_evaluation_on_single_gpu(
+            model=model, device=device, checkpoint_path=checkpoint_path, flag="testing"
+        )
+        evaluate_model_on_single_gpu(
+            model=model,
+            device=device,
+            data_loader=test_loader,
+            report_path=test_report_path,
+            csv_path=test_csv_path,
+            checkpoint_path=checkpoint_path,
+            flag="testing",
+        )
+    else:
+        accelerator.print("Using MegatronLM for evaluation across multiple GPUs!")
+        model, accelerator = prepare_model_for_evaluation_on_multiple_gpu(
+            model=model,
+            device=device,
+            accelerator=accelerator,
+            accelerate_checkpoint_dir=accelerate_checkpoint_dir,
+            flag="validation",
+        )
+        model, val_loader = accelerator.prepare(model, val_loader)
+        evaluate_model_on_multiple_gpu(
+            model=model,
+            device=device,
+            accelerator=accelerator,
+            data_loader=val_loader,
+            report_path=val_report_path,
+            csv_path=val_csv_path,
+            accelerate_checkpoint_dir=accelerate_checkpoint_dir,
+            flag="validation",
+        )
+        model, accelerator = prepare_model_for_evaluation_on_multiple_gpu(
+            model=model,
+            device=device,
+            accelerator=accelerator,
+            accelerate_checkpoint_dir=accelerate_checkpoint_dir,
+            flag="testing",
+        )
+        model, test_loader = accelerator.prepare(model, test_loader)
+        evaluate_model_on_multiple_gpu(
+            model=model,
+            device=device,
+            accelerator=accelerator,
+            data_loader=test_loader,
+            report_path=test_report_path,
+            csv_path=test_csv_path,
+            accelerate_checkpoint_dir=accelerate_checkpoint_dir,
+            flag="testing",
+        )
 
 
 if __name__ == "__main__":
